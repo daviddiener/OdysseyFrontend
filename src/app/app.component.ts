62e956fb22fac0d8992c12f22f5b6c880892313f
David Diener <daviddiener@outlook.com>
2021-01-26 19:30:59 +0100

    added version number in navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from './services/authentication.service';
+import { version } from '../../package.json';
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,6 @@ import { AuthenticationService } from './services/authentication.service';
 })
 export class AppComponent {
   public isCollapsed = true;
+  public version: string = version;
   constructor(public auth: AuthenticationService) {}
 }
