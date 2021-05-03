49fe683d3e8e1b7025ae7fec63c396b26aa2f8f6
David Diener <daviddiener@outlook.com>
2021-05-03 02:03:22 +0200

    fixed environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://odyssey.olympus-studios.com/api/'
+  apiEndpoint: 'http://odyssey.olympus-studios.com/api/',
+  wsEndpoint: 'http://odyssey.olympus-studios.com'
+
 }
